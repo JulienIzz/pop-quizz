41653ab806a9e86c35b99d6ffdae578b83b5c2e9
JulienIz <izzillj@colas.com>
2022-06-30 11:40:30 +0200

    Home cleaning ready to dev

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, TouchableHighlight, TouchableOpacity, Text} from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Text} from 'react-native';
 
 export default function App() {
   return (
     <View style={homeStyles.root}>
-      <TouchableOpacity onPress={() => alert('Waouw')} underlayColor="white">
-          <View style={homeStyles.button}>
-            <Text>JOUER</Text>
-          </View>
-        </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -21,15 +17,4 @@ const homeStyles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  button: {
-    height: 50,
-    width: 200,
-    backgroundColor: '#2D728F',
-    color: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 10,
-    shadowColor: 'black',
-    shadowOpacity: 0.5,
-  }
 });
