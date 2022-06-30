aae32ad8ac3a72dee8458663a1a7e7942a59ada8
JulienIz <izzillj@colas.com>
2022-06-30 11:33:02 +0200

    First Button on Home

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, TouchableHighlight, TouchableOpacity, Text} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={homeStyles.root}>
+      <TouchableOpacity onPress={() => alert('Waouw')} underlayColor="white">
+          <View style={homeStyles.button}>
+            <Text>JOUER</Text>
+          </View>
+        </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const homeStyles = StyleSheet.create({
+  root: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3b8ea5',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    height: 50,
+    width: 200,
+    backgroundColor: '#2D728F',
+    color: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    shadowColor: 'black',
+    shadowOpacity: 0.5,
+  }
 });
