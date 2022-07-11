97cd2754e4976317a6662ee8103d76756aa97613
PapyRoach <julien.izzillo@gmail.com>
2022-07-11 14:39:13 +0200

    Début ReactNavigation

--- App.jsx
+++ App.jsx
@@ -1,14 +1,24 @@
-import { HomePage } from './src/pages/HomePage';
-import { ThemePage } from './src/pages/ThemePage';
-import { useState } from 'react';
-import { Theme_Colorful } from './src/Colors';
+import { HomePage } from "./src/pages/HomePage";
+import { ThemePage } from "./src/pages/ThemePage";
+import { HeaderApp } from "./src/HeaderApp";
+import { useState } from "react";
+import { Theme_Colorful } from "./src/Colors";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [theme, setTheme] = useState(Theme_Colorful);
 
-    const [theme, setTheme] = useState(Theme_Colorful);
+  return (
+    //   <HomePage theme={theme} setTheme={setTheme} />
 
-    return (
-        <HomePage theme={theme} setTheme={setTheme} />
-        // <ThemePage theme={theme} setTheme={setTheme} />
-    );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HeaderApp} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
